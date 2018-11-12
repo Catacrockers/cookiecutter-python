9e1caf1ce88170ffd693fc8e9af183602d0615f6
jbarbadillo <javier.barbadillo@gmail.com>
2018-11-12 19:38:07 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='cookiecutter-pypackage',
+    packages=[],
+    version='0.1.0',
+    description='Cookiecutter template for a Python package',
+    author='Catacrokers',
+    license='MIT',
+    author_email='',
+    url='https://github.com/audreyr/cookiecutter-pypackage',
+    keywords=['cookiecutter', 'template', 'package', ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
